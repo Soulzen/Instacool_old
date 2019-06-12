a68cbb699ae0ab07e96f8fb9109912c6b99a3b52
Alberto Navarro <alberto.liria@gmail.com>
2019-06-12 19:05:57 +0200

    S09E52

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Login from './containers/Auth/Login';
 import Register from './containers/Auth/Register';
 import NewsFeed from './containers/NewsFeed';
 import Navbar from './components/Navbar';
+import Profile from './containers/Profile';
 
 class App extends Component {
 	public render() {
@@ -16,6 +17,7 @@ class App extends Component {
 				<Route exact={true} path="/register" component={Register} />
 				<Route path="/app" component={Navbar} />
 				<Route exact={true} path="/app/newsfeed" component={NewsFeed} />
+				<Route exact={true} path="/app/profile" component={Profile} />
 			</div>
 		);
 	}
---
+++ src/containers/Profile/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const style = {
+	container: {
+		padding: '15px'
+	}
+};
+
+export class Profile extends Component {
+	public render() {
+		return <div style={style.container}> Perfil </div>;
+	}
+}
+
+export default Profile;
