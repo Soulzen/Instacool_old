b651d5134db4073c66e86a4293a68d868bbc60e3
Alberto Navarro <alberto.liria@gmail.com>
2019-06-12 22:51:48 +0200

    S10E61

--- src/containers/Auth/Login.tsx
+++ src/containers/Auth/Login.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Card from '../../components/Card';
 import Container from '../../components/Container';
 import Title from '../../components/Title';
 import LoginForm from '../../components/LoginForm';
+
+import { login } from '../../ducks/Users';
 
 export class Login extends Component {
 	render() {
@@ -18,4 +21,10 @@ export class Login extends Component {
 	}
 }
 
-export default Login;
+const mapStateToProps = (state: any) => state;
+
+const mapDispatchToProps = (dispatch: any) => ({
+	login: (payload: any) => dispatch(login(payload))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
