df7a45a7cc0435e30d1f433cc56f009793d36dbc
Alberto Navarro <alberto.liria@gmail.com>
2019-06-15 12:58:51 +0200

    S11E62 - Parche Cutre

--- src/ducks/Users.ts
+++ src/ducks/Users.ts
@@ -2,15 +2,15 @@ import { Dispatch } from 'redux';
 import { IServices } from '../services';
 
 export interface ILogin {
-	email: string;
-	password: string;
+	email?: string;
+	password?: string;
 }
 
 export default function reducer(state = {}) {
 	return state;
 }
 
-export const login = ({ email, password }: ILogin) => async (
+export const login = ({ email = '', password = '' }: ILogin) => async (
 	dispatch: Dispatch,
 	getState: () => any,
 	{ auth }: IServices
